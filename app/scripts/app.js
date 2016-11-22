4abf961a9804566e648e90c235a7715bdbfbdadb
c0505 <chenmh@dtdream.com>
2016-11-22 17:27:02 +0800

    基线版本

---
+++ app/scripts/app.js
@@ -0,0 +1,67 @@
+
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testChenApp
+ * @description
+ * # testChenApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testChenApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    //'ngRoute',
+    'ui.router',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('main', {
+        url: '/main',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('/',{
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html',
+        controller: ''
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'views/contact.html',
+        controller: ''
+      })
+
+  })
+
+
+
+//ng-route的写法
+  //.config(function ($routeProvider) {
+  //  $routeProvider
+  //    .when('/', {
+  //      templateUrl: 'views/main.html',
+  //      controller: 'MainCtrl',
+  //      controllerAs: 'main'
+  //    })
+  //    .when('/about', {
+  //      templateUrl: 'views/about.html',
+  //      controller: 'AboutCtrl',
+  //      controllerAs: 'about'
+  //    })
+  //    .otherwise({
+  //      redirectTo: '/'
+  //    });
+  //});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testChenApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testChenApp
+ */
+angular.module('testChenApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
