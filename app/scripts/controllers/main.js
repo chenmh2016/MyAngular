d84d2b121b48046be91e80f1e10f72124cea2d5d
c0505 <chenmh@dtdream.com>
2016-12-13 18:01:05 +0800

    增加sidebar的controller，sidebar可以收缩回去，但是展开还有问题

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,57 +9,11 @@
  */
 angular.module('testChenApp')
   .controller('MainCtrl', function ($scope,tree) {
-    //this.awesomeThings = [
-    //  'HTML5 Boilerplate',
-    //  'AngularJS',
-    //  'Karma'
-    //];
-    //var　vm=this;
-    $scope.items=[
-      {
-        id: 1,
-        title: '图书馆',
-        items:[
-          {
-            id:11,
-            title:"用户注册",
-            url:"#/reader/create",
-            state:"reader.create",
-            groupIndex:1,
-            icon:"yundunicon-over-3"
-          },{
-            id:12,
-            title:"图书列表",
-            state:"reader.list",
-            groupIndex:2,
-            icon:"yundunicon-emer-3"
-          },{
-            id:13,
-            title:"主题树",
-            state:"reader.themeTree",
-            groupIndex:3,
-            icon:"yundunicon-thre-3"
-          }
-        ]
-      },{
-        id:2,
-        title:"主题二",
-        poster:"破浪",
-        dateCreated:"2015-08-07T15:05:06",
-        items:[
-          {
-            id:11,
-            title:"主题二",
-            url:"#",
-            state:"reader.create",
-            groupIndex:4,
-            icon:"yundunicon-vuln-3"
-          }
-        ]
-      }
-    ];
-    tree.enhance($scope.items);
-    $scope.select = function (data) {
-        $scope.isActive = data.groupIndex;
-    };
+
+    $scope.aaa="Dfdf";
+    $scope.$on('sidebarFold', function (event,data) {
+      $scope.isSidebarFold=data;
+      console.log($scope.isSidebarFold+"isSidebarFold");
+    })
+
   });
---
+++ app/scripts/controllers/sidebar.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testChenApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testChenApp
+ */
+angular.module('testChenApp')
+  .controller('SideBarCtrl', function ($scope,tree) {
+
+    $scope.items=[
+      {
+        id: 1,
+        title: '图书馆',
+        items:[
+          {
+            id:11,
+            title:"用户注册",
+            url:"#/reader/create",
+            state:"reader.create",
+            groupIndex:1,
+            icon:"yundunicon-over-3"
+          },{
+            id:12,
+            title:"图书列表",
+            state:"reader.list",
+            groupIndex:2,
+            icon:"yundunicon-emer-3"
+          },{
+            id:13,
+            title:"主题树",
+            state:"reader.themeTree",
+            groupIndex:3,
+            icon:"yundunicon-thre-3"
+          }
+        ]
+      },{
+        id:2,
+        title:"主题二",
+        poster:"破浪",
+        dateCreated:"2015-08-07T15:05:06",
+        items:[
+          {
+            id:11,
+            title:"主题二",
+            url:"#",
+            state:"reader.create",
+            groupIndex:4,
+            icon:"yundunicon-vuln-3"
+          }
+        ]
+      }
+    ];
+    tree.enhance($scope.items);
+    $scope.select = function (data) {
+      $scope.isActive = data.groupIndex;
+    };
+    $scope.isSidebarFold=false;
+    function toggleSidebarStatus(){
+      $scope.isSidebarFold = !$scope.isSidebarFold;
+      var type = $scope.isSidebarFold?'mini':'full';
+      console.log($scope.isSidebarFold+"$scope.isSidebarFold");
+      $scope.$emit("sidebarFold",[$scope.isSidebarFold]);
+    }
+
+    $scope.toggleSidebarStatus = toggleSidebarStatus;
+
+  });
